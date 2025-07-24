567077fd8a58f71dd005783c30878b6bf7456c8c
wolframaleph <wolframaleph@gmail.com>
2025-07-24 06:13:31 -0500

    GSTBS: Clean stub files

--- worlds/gstbs/gsutils/class_data.py
+++ worlds/gstbs/gsutils/class_data.py
@@ -1,12 +1,4 @@
-import struct
-import pickle
-from dataclasses import dataclass
 from typing import TYPE_CHECKING
-import orjson
-
-from .binary_utils import Rom, ABILITY_STRUCT_FMT, ABILITY_STRUCT_LEN
-from .text_utils import read_line, format_line, GSTBSInternalStringData
-
 if TYPE_CHECKING:
     from pathlib import Path
 
